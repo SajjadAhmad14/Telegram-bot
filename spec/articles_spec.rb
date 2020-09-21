01f08e8cc889b1f559243a82b6645d77c5926d3e
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-21 19:33:25 +0500

    add specs for #random_article and article instance

---
+++ spec/articles_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/articles'
+# require_relative '../lib/bot'
+
+describe Atricles do
+  let (:article) { Atricles.new }
+  describe '#initialize' do
+    it "create an instance of Articles" do
+      expect(article).to be_a Atricles
+    end
+  end
+
+  describe '#random_article' do
+    it "return a link as a String" do
+      expect(article.random_article).to be_a String
+    end
+  end
+end
