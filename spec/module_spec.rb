5f91ea69f1fe22b240bcc8d8644c4416221e1961
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-21 21:00:30 +0500

    add spec for rand_string of Methods

---
+++ spec/module_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/module'
+
+describe Methods do
+  describe "#rand_string" do
+    it "return a random element" do
+      arr = [1, 2, 3]
+      expect(Methods.rand_string(arr)).to be_an Integer
+    end
+  end
+end
