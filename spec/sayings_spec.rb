e29f505ce93d4d4b42758edd59e65e04b66a879c
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-22 13:07:07 +0500

    add spec which checks that instance is created

--- spec/sayings_spec.rb
+++ spec/sayings_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../lib/sayings'
 
 describe Sayings do
+  let (:saying) {Sayings.new}
+  describe '#initialize' do
+    it "create instance of Sayings" do
+      expect(saying).to be_a Sayings
+    end
+  end
 end
