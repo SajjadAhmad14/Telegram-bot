f640b1f8978cfcbd4eb2e3008d4bf0ebc6bc1944
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-22 12:51:47 +0500

    add descriptive notes when /satrt and /stop

--- lib/bot.rb
+++ lib/bot.rb
@@ -8,13 +8,13 @@ class MyBot
     bot.listen do |message|
       case message.text
       when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Welcome #{message.from.first_name},to my bot")
+        bot.api.send_message(chat_id: message.chat.id, text: "Welcome, #{message.from.first_name},to my bot. You may enter ./article for a programming article to read, /saying for a programming saying, /joke for  a joke or /stop to stop the bot!")
       when '/stop'  
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
+        bot.api.send_message(chat_id: message.chat.id, text: "Don't say bye, #{message.from.first_name}!", date: message.date)
       when '/article'
         article = Atricles.new
         bot.api.send_message(chat_id: message.chat.id, text: article.random_article)
-      else bot.api.send_message(chat_id: message.chat.id, text: "Invalid command, #{message.from.first_name}, please enter /start and /stop")
+      else bot.api.send_message(chat_id: message.chat.id, text: "Invalid command, #{message.from.first_name}, Please enter /start, /stop or /article")
       end
     end
   end
