7e7ce2c31fd5444f9142eeed3d367dfbf4a9f513
Sajjad Ahmad <jogimar14@gmail.com>
2020-09-23 18:14:38 +0500

    fix rubocop errors

--- lib/reuse.rb
+++ lib/reuse.rb
@@ -1,4 +1,4 @@
-#This Module has method which return a random element is reused in other class
+# This Module has method which return a random element is reused in other class
 module Methods
   def self.rand_string(array)
     array[rand(array.size)]
