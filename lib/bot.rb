8e97633f47be9764a587a841e047b789670ca3d3
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-18 21:46:38 +0500

    random_joke returns a joke after searching it by a word

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,24 +4,28 @@ require_relative './sayings'
 require_relative './jokes'
 require 'dotenv'
 Dotenv.load
-# A class which has switch statements to check which command is entered
+# A class which has conditional statements to check which command is entered
 class MyBot
+  attr_reader :text
   Telegram::Bot::Client.run(ENV['TELEGRAM_KEY'], logger: Logger.new($stderr)) do |bot|
     bot.listen do |message|
-      case message.text
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Welcome, #{message.from.first_name},to my bot. You may enter /article for a programming article to read, /saying for a programming saying, /joke for a programming joke or /stop to stop the bot!")
-      when '/stop'
+      if message.text == '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Welcome, #{message.from.first_name},to my bot. You may enter /article for a programming article to read, /saying for a programming saying, any word to search a joke about or /stop to stop the bot!")
+      elsif message.text == '/stop'
         bot.api.send_message(chat_id: message.chat.id, text: "Don't say bye, #{message.from.first_name}!", date: message.date)
-      when '/article'
+      elsif message.text == '/article'
         article = Atricles.new
         bot.api.send_message(chat_id: message.chat.id, text: article.random_article)
-      when '/saying'
+      elsif message.text == '/saying'
         saying = Sayings.new
         bot.api.send_message(chat_id: message.chat.id, text: saying.random_saying)
-      when '/joke'
-        joke = Jokes.new
-        bot.api.send_message(chat_id: message.chat.id, text: joke.random_joke)
+      elsif message.text.match(/^[[:alpha:]]+$/)
+        t = message.text
+        if Jokes.new.random_joke(t) == nil
+          bot.api.send_message(chat_id: message.chat.id, text: 'Please try another word!')
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: Jokes.new.random_joke(t))
+        end
       else bot.api.send_message(chat_id: message.chat.id, text: "Invalid command, #{message.from.first_name}, Please enter /start, /stop, /article, /saying or /joke")
       end
     end
--- lib/jokes.rb
+++ lib/jokes.rb
@@ -1,22 +1,8 @@
 require_relative './reuse'
-# A class which has an instance variable of strings an instance method
+require 'dad_jokes'
+# A class with a method which returns a joke after searching it by word! 
 class Jokes
-  include Methods
-  attr_reader :programming_jokes
-  def initialize
-    @programming_jokes = ['A database administrator left his wife because she had one-to-many relationships',
-                          'When a cat or a programmer is happy and excited,
-                          an appropriate question would be, did you find a bug?',
-                          'Assembly programmers always soaking wet because They work below C-level',
-                          "programmer quit his job because he didn't get arrays",
-                          'The object-oriented way to become wealthy is Inheritance',
-                          'Programming is like sex: One mistake and you have to support it for the rest of your life.',
-                          "Progarmmer is a person who fixed a problem that you don't know you have, in a way you don't understand!",
-                          'The cheapest, fastest, and most reliable components are those that aren’t there.',
-                          'A programmer is told to go to hell, he finds the worst part of that statement is the go to']
-  end
-
-  def random_joke
-    Methods.rand_string(@programming_jokes)
+  def random_joke(text)
+    DadJokes.search(term: text, limit: 1)[0]
   end
 end
